f2d7dc617985f7c9d322486885d0815c9d9edc89
Samikkannu <jayanthi.samikkannu@dxc.com>
2020-07-18 00:07:25 +0530

    Added login and form details

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import logo from "./logo.svg";
+import "./App.css";
+//import { Container, Row, Col } from "reactstrap";
+
+//import Manager from "./Manager/manager";
+//import Header from "./Shared/Header/header"
+import ResourceForm from './Resource/resourceForm';
 
 function App() {
   return (
+    <React.Fragment>
+  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <ResourceForm />
     </div>
+    </React.Fragment>
   );
 }
 
